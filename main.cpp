cc17598701492f0e46c44c12c44eb3772d3162bd
Mike <mjbonney78@gmail.com>
2017-04-01 08:04:19 -0700

    modify comments

--- main.cpp
+++ main.cpp
@@ -41,8 +41,8 @@ int main(int argc, char *argv[]) {
 			else
 				i++;
 		}
-		currentFloor = nums[0];				
-		nums.erase(nums.begin());			//delete first item in vector 
+		currentFloor = nums[0];				//assign starting position to currentFloor then delete from vector
+		nums.erase(nums.begin());			
 		
 		string mode = argv[2];
 		if (mode.compare("A") == 0)
@@ -50,7 +50,6 @@ int main(int argc, char *argv[]) {
 		else
 			modeB(nums, currentFloor);
 	}
-	
 
 	return 0;
 }
@@ -72,21 +71,21 @@ void modeA(vector<int> &requests, int current) {
 
 void modeB(vector<int> &requests, int current) {	
 	int total = 0;
-	vector<int> stops;																//to hold sequence fo floors stopped at
-	queue<pair<int, int>> rides;													//to hold each ride request.  first = origin, second = destination
+	vector<int> stops;								//to hold floors stopped at in sequential order
+	queue<pair<int, int>> rides;					//to hold each ride request.  first = origin, second = destination
 
-	stops.push_back(current);														//add starting floor to stops
+	stops.push_back(current);						//add starting floor to stops
 
-	for (int i = 0; i < requests.size(); i += 2)									//create queue
+	for (int i = 0; i < requests.size(); i += 2)						//create queue
 		rides.push(make_pair(requests[i], requests[i + 1]));
 
 	while (!rides.empty()) {		
-		vector<int> temp;															//to hold all origins and destination of consecutive rides going in same direction
+		vector<int> temp;							//to hold all origins and destination of consecutive rides going in same direction
 		temp.push_back(rides.front().first);
 		temp.push_back(rides.front().second);
-		rides.pop();																//pop off first ride, add to temp
+		rides.pop();								//pop off first ride, add to temp
 
-		if (temp[0] < temp[1]){															//going up			
+		if (temp[0] < temp[1]){				//going up			
 			while (!rides.empty() && rides.front().first < rides.front().second) {		//look ahead in queue and add to temp if also going up
 				temp.push_back(rides.front().first);
 				temp.push_back(rides.front().second);
@@ -94,7 +93,7 @@ void modeB(vector<int> &requests, int current) {
 			}
 			sort(temp.begin(), temp.end());			
 		}
-		else {																			//going down	
+		else {								//going down	
 			while (!rides.empty() && rides.front().first > rides.front().second) {		//add each consecutive ride that is also going down
 				temp.push_back(rides.front().first);
 				temp.push_back(rides.front().second);
@@ -103,13 +102,13 @@ void modeB(vector<int> &requests, int current) {
 			sort(temp.begin(), temp.end(), [](const int a, const int b) {return a > b; });		//sort in reverse in order to use same logic below	
 		}
 
-		if(temp[0] != current)															//only add to stop if elevator currently not at first ride start position
+		if(temp[0] != current)						//only add to stop if elevator currently not at first ride start position
 			stops.push_back(temp[0]);
-		for (int i = 1; i < temp.size(); i++) {											//add unique floors to stops in current direction
+		for (int i = 1; i < temp.size(); i++) {		//add unique floors to stops in current direction
 			if (temp[i] != temp[i - 1])
 				stops.push_back(temp[i]);
 		}
-		current = temp[temp.size() - 1];												//update current elevator position to that of last ride destination in current direction
+		current = temp[temp.size() - 1];			//update current elevator position to that of last ride destination in current direction
 	}
 	for (int i = 0; i < stops.size(); i++) {											
 		if (i > 0)
